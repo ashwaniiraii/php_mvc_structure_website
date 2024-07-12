c189b49f26472c093177e0e283af6c9ff0efde71
ashwaniiraii <ashwanirai975@gmail.com>
2024-07-12 18:18:18 +0530

    changes

--- index.php
+++ index.php
@@ -7,8 +7,10 @@ $dsn = 'mysql:host=localhost;dbname=myapp;charset=utf8';
 $username = 'root';
 $password = '';
 $pdo = new PDO($dsn, $username, $password);
-$statement = $pdo->prepare('select * from posts');
+$statement = $pdo->prepare('select * from posts where id=1');
 $statement->execute();
-$posts  = $statement->fetchAll();
-dd($posts);
+$posts  = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach($posts as $post){
+    echo '<li>'.$post['title'].'</li>';
+}
 
