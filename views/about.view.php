5cc1f3a3c2ca6ca214716414d66ddb2ff4c70770
ashwaniiraii <ashwanirai975@gmail.com>
2024-07-12 11:16:50 +0530

    deployproject

---
+++ views/about.view.php
@@ -0,0 +1,15 @@
+<?php require('partials/header.php') ?>
+    <div class="min-h-full">
+        <?php require('partials/nav.php')?>
+        <header class="bg-white shadow">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">About Page</h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <p>Hello Welcome to the About Page</p>
+            </div>
+        </main>
+    </div>
+<?php include('partials/footer.php') ?>
---
+++ views/partials/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html class="h-full bg-gray-100">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full">
